9034a49c122af31914438b26b5a8407e2d5aa1b1
smkakitani <82690252+kakisaura@users.noreply.github.com>
2025-09-22 23:14:29 -0300

    test NavBar

--- tests/NavBar.test.jsx
+++ tests/NavBar.test.jsx
@@ -1,16 +1,28 @@
 import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { getByRole, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import NavBar from "../src/components/NavBar";
-import { MemoryRouter, Route, Routes } from "react-router";
+// import { MemoryRouter, Route, Routes } from "react-router";
 import { createMemoryRouter, Router, RouterProvider } from "react-router";
+
 import routes from "../src/app/routes";
 
 
 describe('NavBar component', () => {
-  it('render navbar correctly', () => {
-    // const router = createMemoryRouter(routes, { initialEntries: ["/"] });
+  it('renders navbar correctly', () => {
+    const router = createMemoryRouter(routes/* , { initialEntries: ["/"] } */);
 
-    // render(<RouterProvider router={router}/>);
+    render(<RouterProvider router={router}/>);
+
+    expect(screen.getByRole('navigation'));
+  });
+
+  it('renders 3 links of navbar', () => {
+    const router = createMemoryRouter(routes);
+    
+    render(<RouterProvider router={router}/>);
+
+    expect(screen.getAllByRole('link')).toHaveLength(3);
   });
 });
+
